635ded02ede519d79ce4e7d72d02a19cd0d61a1b
Gregor Trefs <Gregor.Trefs@gmail.com>
2019-03-10 23:15:25 +0100

    Setup with flat dir
    Initial commit with jars in lib folder. The project is set up for JUnit
    5, vavr 0.10.0 and hamcrest 2.1. It is configured to be run with Java
    11.

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+
+repositories {
+    flatDir {
+        dirs("lib")
+    }
+}
+
+dependencies {
+    implementation("io.vavr:vavr:0.10.0")
+    implementation("io.vavr:vavr-match:0.10.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
+    testImplementation("org.hamcrest:hamcrest:2.1")
+    testImplementation("org.apiguardian:apiguardian-api:1.0.0")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
+    testRuntime("org.junit.platform:junit-platform-commons:1.1.0")
+    testRuntime("org.junit.platform:junit-platform-engine:1.1.0")
+    testRuntime("org.opentest4j:opentest4j:1.0.0")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}

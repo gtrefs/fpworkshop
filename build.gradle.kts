5c00cc1c87a5bb23b138c8f93a0be55be09536bd
Gregor Trefs <Gregor.Trefs@gmail.com>
2022-01-04 22:23:01 +0000

    Support Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
-
 repositories {
+    mavenCentral()
     flatDir {
         dirs("lib")
     }
@@ -18,14 +19,9 @@ repositories {
 dependencies {
     implementation("io.vavr:vavr:0.10.0")
     implementation("io.vavr:vavr-match:0.10.0")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testImplementation("org.hamcrest:hamcrest:2.1")
     testImplementation("org.apiguardian:apiguardian-api:1.0.0")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
-    testRuntime("org.junit.platform:junit-platform-commons:1.1.0")
-    testRuntime("org.junit.platform:junit-platform-engine:1.1.0")
-    testRuntime("org.opentest4j:opentest4j:1.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 tasks.named<Test>("test") {

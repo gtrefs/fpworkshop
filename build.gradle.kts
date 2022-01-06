9735497c0ede73e92e985fffe220cfdc1427bb98
Gregor Trefs <Gregor.Trefs@gmail.com>
2022-01-06 22:20:46 +0000

    Added jqwik

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,15 @@ dependencies {
     testImplementation("org.hamcrest:hamcrest:2.1")
     testImplementation("org.apiguardian:apiguardian-api:1.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("net.jqwik:jqwik:1.6.2")
+     testImplementation("org.assertj:assertj-core:3.12.2")
 }
 
 tasks.named<Test>("test") {
-    useJUnitPlatform()
+    useJUnitPlatform {
+        includeEngines("jqwik", "junit-jupiter")
+    }
+    include("**/*Properties.class")
+	include("**/*Test.class")
+	include("**/*Tests.class")
 }

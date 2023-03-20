ddd8b563a130dd5092813ecbf14e5e6a2f8a97f4
gtrefs <gtrefs@liveperson.com>
2023-03-20 01:15:56 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,11 +19,11 @@ repositories {
 dependencies {
     implementation("io.vavr:vavr:0.10.0")
     implementation("io.vavr:vavr-match:0.10.0")
-    testImplementation("org.hamcrest:hamcrest:2.1")
+    testImplementation("org.hamcrest:hamcrest:2.2")
     testImplementation("org.apiguardian:apiguardian-api:1.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("net.jqwik:jqwik:1.6.2")
-     testImplementation("org.assertj:assertj-core:3.12.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("net.jqwik:jqwik:1.7.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.named<Test>("test") {
